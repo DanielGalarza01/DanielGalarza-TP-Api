1ac23415517301b24e0b68692208a4edf261b4ef
DanielGalarza01 <galarzadanieleduardo@gmail.com>
2022-06-13 05:18:18 -0300

    cuarto

---
+++ DanielGalarza-TP-Api/src/main/java/com/tp2_neoris/DanielGalarzaTpApiApplication.java
@@ -0,0 +1,13 @@
+package com.tp2_neoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielGalarzaTpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielGalarzaTpApiApplication.class, args);
+	}
+
+}
---
+++ DanielGalarza-TP-Api/src/main/java/com/tp2_neoris/repository/EmpleadoRepository.java
@@ -0,0 +1,11 @@
+package com.tp2_neoris.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tp2_neoris.entity.EmpleadoEntity;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<EmpleadoEntity, Long>{
+
+}
